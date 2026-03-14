4f39be26688076fcacf288c3ad3fabf6a9c2b63c
yigit2403 <yigitharun24@gmail.com>
2026-03-14 20:28:48 +0300

    Fixed the tooth selection ui

---
+++ ViewModels/ToothSelectorViewModel.cs
@@ -0,0 +1,10 @@
+namespace DentistDB.ViewModels;
+
+public class ToothSelectorViewModel
+{
+    public string Label { get; set; } = "Dis Secimi";
+    public string InputId { get; set; } = "SelectedTeeth";
+    public string InputName { get; set; } = "SelectedTeeth";
+    public string SummaryId { get; set; } = "SelectedTeethSummary";
+    public string SelectedTeethCsv { get; set; } = string.Empty;
+}
